c359f25c8ab6f7c1a7f81d5236e07abcfddb77c2
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2026-03-15 13:04:33 -0400

    add mor samples and documentation

--- introduction_dj/introduction_python/basic_consept.md
+++ introduction_dj/introduction_python/basic_consept.md
@@ -1,3 +1,14 @@
+# Scoope
+The scoope in Python is like the ***yalm*** file
+```
+ if(true):
+     do something
+     do another thing 
+     do more of this scoope
+
+ do something out of the scoope    
+```
+
 # How to work the class in python
 For example if you whant create a for using a sequense and push CTRL + click in the function you will see
 something like this 
@@ -27,4 +38,17 @@ And if you use tree argument is like this:
 ```py
 for i in range(1,10,2): # Return: 1,3,5,7,9
     print(i)
+```
+
+# Function
+The function in python don't requered are inside a class like kotlin
+
+## Sintaxis
+```py
+ def function_name(a, b, c):
+    return b * b - 4 * a * c 
+```
+## Call the function
+```py
+function_name(2,4,3) # this function return the discriminant
 ```
--- introduction_dj/introduction_python/basic_consept.py
+++ introduction_dj/introduction_python/basic_consept.py
@@ -1,24 +1,32 @@
 # Dinamic type
+
+print("===============================  Dinamic Type")
 a = 10
 b = 20.5
-c = a +b
+c = a + b  
 print(a)
-a = 'I am a string'
+a = 'I am a string'   
 print(a)
+
+
+
 
 
 # Do not exist the char 
 
+print("===============================  String")
 string1 = "I am a string"
 string2 = 'I am a string'
 string3 = 'a'  #This is a string too
 
 
 # List
+print("===============================  List")
 list = [1,2,3,4,5,10]
+print(list[1])  # Return 2
 
 # For
-
+print("===============================  For")
 for i in list: # Return: 1,2,3,4,5,10
     print(i)
 
@@ -28,4 +36,34 @@ for i in range(1,4):  # Return: 1,2,3
 for i in range(1,10,2): # Return: 1,3,5,7,9
     print(i)
 
+# If    
+print("===============================  If")
+oper1 =4
+oper2 =4
+
+if(oper1 == oper2):
+    print("Are equals")
+
+if(oper1 != oper2):
+    print("Are Diferent")
+
+if(oper1 >= oper2):
+    print("Are > equals")
+    
+elif(oper1 <= oper2):
+    print("Are < equals")
+
+if(oper1 > oper2):
+    print("Are >")
+
+else: 
+    print("Are <")
+
+# While
+print("===============================  If")
+itereator = 0
+
+while (itereator<10):
+    print(itereator)
+    itereator= itereator+1    
 
